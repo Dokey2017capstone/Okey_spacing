010a6213e098b2d279132904f0e1d974531ebf46
heojoung2 <heojoung2@gmail.com>
2017-04-14 14:54:31 +0900

    Update 3_accuracy.py

--- spacing/3_accuracy.py
+++ spacing/3_accuracy.py
@@ -36,22 +36,43 @@ def result_function(sentence, tag):     #출력스트링
     return result
 
 
-def accuracy_function(result_list):     #정확도
-    index = -1
-    cnt = 0
+def answer_tag_function(answer_file):       #정답 스트링에 태그만을 다는 함수
+    answer_tag_list = []
+    first_flag = False  # 첫 문장 공백
 
-    answer_file = open('answer.txt', 'r', encoding='utf-8')
     for sentence in answer_file:
-        if index == -1:
-            pass
-        else:
-            if sentence.strip() == result_list[index]:
-                cnt += 1
-        index += 1
-    answer_file.close()
+        if first_flag:
+            sentence = sentence.lstrip()
 
-    accuracy = (cnt / float(index)) * 100
-    return accuracy
+            if not (len(sentence) == 1 or len(sentence) == 0):  # 빈 줄 제외
+                tag = []
+
+                for i in range(len(sentence)):
+                    if i == 0:
+                        tag.append(str('1'))
+                    elif i == (len(sentence) - 1):  # 맨 뒤 unicode인식
+                        pass
+                    elif sentence[i] != ' ' and sentence[i - 1] == ' ':  # 첫 음절일때
+                        tag.append(str('1'))
+                    elif sentence[i] != ' ' and sentence[i - 1] != ' ':  # 아닐 때
+                        tag.append(str('0'))
+                answer_tag_list.append(tag)
+        first_flag = True
+    return answer_tag_list
+
+
+def syllable_accuracy_function(result_tag_list,answer_tag_list):    #음절 정확도
+    cnt=0
+    all=0
+    for i,j in zip(result_tag_list,answer_tag_list):
+        for k in range(len(i)):
+            if i[k]==j[k]:
+                cnt+=1
+            all+=1
+    return cnt/float(all)*100
+
+def word_accuracy_function(result_tag_list,answer_tag_list):
+    pass
 
 
 test_file = open('test.txt','r',encoding='utf-8')         #파일 전체 읽기
@@ -60,25 +81,29 @@ result_file = open('result.txt','w',encoding='utf-8')
 crf_tagger = pycrfsuite.Tagger()  # Taggger 클래스는 crf모델을 사용하여 입력 시퀀스의 레이블 시퀀스를 예측하는 기능을 제공한다.
 crf_tagger.open('crf.crfsuite')  # 모델 파일을 연다
 
-result_list=[]
+result_tag_list=[]
 first_flag = False
 for sentence in test_file:
-
-    sentence = sentence.strip()
-
-    feature_string = feature_function(sentence)     #특징함수적용
-    tag = crf_tagger.tag(feature_string)    # 훈련된 모델의 항목 순서에 대한 레이블 순서를 예측
-    result = result_function(sentence, tag)     #결과도출
-
     if first_flag:
+        sentence = sentence.strip()
+        feature_string = feature_function(sentence)     #특징함수적용
+        tag = crf_tagger.tag(feature_string)    # 훈련된 모델의 항목 순서에 대한 레이블 순서를 예측
+        result_tag_list.append(tag)
+
+        result = result_function(sentence, tag)     #결과도출
         result_file.write(result + '\n')
-        result_list.append(result)
     first_flag=True
 
+
 #정답과 비교
-print 'accuracy :',accuracy_function(result_list)
+answer_file = open('answer.txt', 'r', encoding='utf-8')
+answer_tag_list = answer_tag_function(answer_file)
+
+print 'syllable accuracy :',syllable_accuracy_function(result_tag_list,answer_tag_list)
+#print 'word accuracy :',word_accuracy_function(result_tag_list,answer_tag)
 
 crf_tagger.close()  # 모델 닫기
 test_file.close()
 result_file.close()
-#answer_file은 첫줄공백
+answer_file.close()
+#test_file, answer_file은 첫줄공백
